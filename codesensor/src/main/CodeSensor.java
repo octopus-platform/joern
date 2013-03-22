09e3d969b81c621a67127ecbeb1b4eb0eddf6e64
fabs <fabianyamaguchi@gmx.net>
2013-03-22 20:10:00 +0100

    Undid changes I made for concurrency. This commit is clean again.

--- codesensor/src/main/CodeSensor.java
+++ codesensor/src/main/CodeSensor.java
@@ -1,37 +1,49 @@
 package main;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
 import main.CommandLine.CommandLineInterface;
+import main.ModuleParser.ModuleParser;
 
 
 public class CodeSensor {
-	
-	
+    
+	private static ModuleParser parser = new ModuleParser();
 	private static CommandLineInterface cmd = new CommandLineInterface();
-	private static ConcurrentLinkedQueue<FileToParse> queue = new ConcurrentLinkedQueue<FileToParse>();
-	private static String[] userSpecifiedFiles;
 	
     public static void main(String[] args)
 	{
     	cmd.parseCommandLine(args);
-    	userSpecifiedFiles = cmd.getUserSpecifiedFiles();  	    	
     	
-    	createProducerThread();
-    	createConsumerThreads();
+    	try{
+	    	List<String> filesToProcess = cmd.getFilesToProcess();
+	    	processListOfFiles(filesToProcess);
+	    }catch(IOException err){
+			System.err.println("I/O-Error: " + err.getMessage()); 
+	    }
+
 	}
 
-	private static void createProducerThread()
+	private static void processListOfFiles(List<String> filesToProcess)
 	{
-		// new Thread(new FilenameProducer(queue, userSpecifiedFiles)).start();
-		new FilenameProducer(queue, userSpecifiedFiles).run();
+		for(Iterator<String> i = filesToProcess.iterator(); i.hasNext();)
+		{
+			String filename = i.next();
+			processSingleFile(filename);
+		}
 	}
-
-	private static void createConsumerThreads()
-	{
-		for(int i = 0; i < 2; i++)
-			new Thread(new FilenameConsumer(queue)).start();
-		// new FilenameConsumer(queue).run();
-	}
-	    
+	
+    private static void processSingleFile(String filename)
+    {
+    	try{
+    		System.out.println(filename);
+    		parser.parseAndWalkFile(filename);
+    	}catch(IOException ex){
+    		System.err.println("Error processing file: " + filename);
+    	}
+    }
+    
+    
 }
--- codesensor/src/main/CommandLine/CommandLineInterface.java
+++ codesensor/src/main/CommandLine/CommandLineInterface.java
@@ -41,9 +41,36 @@ public class CommandLineInterface
 		formater.printHelp("codesensor [OPTION]... [FILE] ...", options);
 	}
 
-	public String[] getUserSpecifiedFiles()
+	public List<String> getFilesToProcess() throws IOException
 	{
-		return filenames;
+		List<String> retList = new LinkedList<String>();
+		
+		for(int i = 0; i < filenames.length ; i++){
+			
+			String filename = filenames[i];
+			File file = new File(filename);
+			
+			if(!file.exists()){
+				System.err.println("Warning: " + filename + ": no such file or directory");
+				continue;
+			}
+			
+			if(file.isDirectory()){
+				List<String> fs = getFilesInDirectory(filename);
+				retList.addAll(fs);
+			}else{
+				retList.add(filename);
+			}
+		}
+		return retList;
+	}
+	
+	private List<String> getFilesInDirectory(String filename) throws IOException
+	{
+		DirectoryWalker walker = new DirectoryWalker("*.{c,cpp,h,cc,hpp}");
+		Path dir = Paths.get(filename);
+		Files.walkFileTree(dir, walker);
+		return walker.filenames;
 	}
 	
 	public void parseCommandLine(String [] args)
@@ -59,3 +86,4 @@ public class CommandLineInterface
 	}
 	
 }
+
