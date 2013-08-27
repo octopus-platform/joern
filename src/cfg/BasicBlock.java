17e19fdd134d26013ef72793db0f32a5c8286298
fabs <fyamagu@gwdg.de>
2013-08-27 12:17:23 +0200

    Fix for change to BasicBlock.

--- src/cfg/BasicBlock.java
+++ src/cfg/BasicBlock.java
@@ -1,13 +1,9 @@
 package cfg;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import astnodes.ASTNode;
 
 public class BasicBlock {
 
-	List<ASTNode> statements = new LinkedList<ASTNode>();
 	ASTNode astNode;
 	
 	public void setASTNode(ASTNode node)
@@ -22,16 +18,18 @@ public class BasicBlock {
 
 	public String getEscapedCodeStr()
 	{
-		if(statements.size() == 0 || statements.get(0) == null)
+		if(astNode == null)
 			return "";
-		return statements.get(0).getEscapedCodeStr();
+		
+		return astNode.getEscapedCodeStr();
 	}
 
 	public String getType()
 	{
-		if(statements.size() == 0 || statements.get(0) == null)
+		if(astNode == null)
 			return "";
-		return statements.get(0).getTypeAsString() + "BB";
+
+		return astNode.getTypeAsString() + "BB";
 	}
 
 }
