ee9d2aee7897051f2d7ffa801b1989ea8a03f4e3
fabs <fyamagu@gwdg.de>
2013-09-26 17:51:16 +0200

    startNodeSelection steps isolated.

---
+++ libjoern/python/joernsteps/startNodeSelection.groovy
@@ -0,0 +1,143 @@
+
+import java.util.regex.*;
+import com.tinkerpop.blueprints.pgm.impls.neo4j.util.*;
+
+//////////////////////////////////////////////////////////////////////
+// Start Node Selection (Coarse Node Selection)
+// The following are utility functions for Gremlin/Groovy scripts,
+// which you can use to select start-nodes for your analysis. For
+// example, you might want to start with all function definitions, all
+// calls to a specific function or all instances of variables of a
+// certain type.
+//////////////////////////////////////////////////////////////////////
+
+/**
+   Allows more than one pipe to generate starting points. Acts as an
+   OR.
+*/
+
+Object.metaClass.OR = { pipes ->
+  np = []
+  for(int i = 0; i < pipes.size(); i++){
+    p = pipes[i]
+    np.add( _().transform{ p }.scatter())
+  }
+  _().copySplit( *np ).exhaustMerge()
+}
+
+/**
+   Query the node index.
+   @param query Lucene query
+*/
+
+Object.metaClass.queryNodeIndex = { query ->
+  index = g.getRawGraph().index().forNodes("nodeIndex")
+  new Neo4jVertexSequence(index.query(query), g)._()
+}
+
+/**
+   Retrieve all nodes of a specified type from the node index.
+   @param typeName type
+*/
+
+Object.metaClass.getNodesByType = { typeName ->
+  g.idx("nodeIndex")[[type:typeName]]
+}
+
+////////////////////////////////
+///// Functions ///////
+///////////////////////////////
+
+/**
+   Retrieve function by name.
+   @param aName name
+*/
+
+Object.metaClass.getFunctionByName = { aName ->
+  g.idx('nodeIndex')[[functionName:aName]]
+}
+
+/**
+   Retrieve function by name matching regular
+   expression.
+   @param aNameRegex regular expression function names must match.
+*/
+
+Object.metaClass.getFunctionByNameRegex = { aNameRegex ->
+  getNodesByType("Function").filter{ it.functionName.matches(aNameRegex) }
+}
+
+//////////////////////////////////
+////// Parameters //////
+/////////////////////////////////
+
+Object.metaClass.getParametersOfType = { typeName ->
+  query = 'type:"ParameterType" AND code:"' + typeName + '"'
+  queryNodeIndex(query)
+}
+
+Object.metaClass.getParameterByRegex = { aNameRegex ->
+  getNodesByType("Parameter").filter{ it.code.matches(aNameRegex) }
+}
+
+Object.metaClass.getParameterByNameRegex = { aNameRegex ->
+  getNodesByType("Parameter").out('IS_AST_PARENT').filter{ it.type == 'Identifier' && it.code.matches(aNameRegex) }
+}
+
+/////////////////////////////
+//////// Calls //////////
+////////////////////////////
+
+/**
+   Retrieve all calls by callee.  This is more efficient than
+   'getCallsToRegex' but less powerful, allowing only exact matches of
+   callee names.
+
+  @param callee identifier of the callee
+*/
+
+Object.metaClass.getCallsTo = { callee ->
+  query = 'type:"CallExpression" AND code:' + callee + '*'
+  queryNodeIndex(query).filter
+  {
+    it.code.startsWith(callee + ' ')
+  }
+}
+
+/**
+   Retrieve all calls by regular expression To match a regex, we can
+   only use the index to extract all CallExpressions. We then need to
+   iterate over all call expressions to see where the regex matches.
+
+   @param callee regular expression callees must match.
+   
+ */
+ 
+Object.metaClass.getCallsToRegex = { callee ->
+  getNodesByType("CallExpression").filterCodeByCompiledRegex(Pattern.compile(callee))
+}
+
+//////////////////////////
+//// Arguments to calls
+//////////////////////////
+
+/**
+   Retrieve n'th arguments to calls by callee.
+   
+   @param callee callee
+   @param n the parameter number (a String)
+*/
+
+Object.metaClass.getArgumentNTo = { callee, n ->
+  getCallsTo(callee).callToArgumentN(n)
+}
+
+
+/**
+   Retrieve declaration by name.
+   @param aName name
+*/
+
+Object.metaClass.getTypeDeclsByName = { aName ->
+  g.idx('nodeIndex')[[name:aName]]
+}
