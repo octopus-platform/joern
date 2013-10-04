32a900c1982ae8e138c465018ee985ec1cdc9eb4
fabs <fyamagu@gwdg.de>
2013-10-05 01:57:04 +0200

    Changed localDecl step to return only IdentifierDecls and no Parameters.

--- libjoern/python/joernsteps/misc.groovy
+++ libjoern/python/joernsteps/misc.groovy
@@ -4,8 +4,12 @@ Gremlin.defineStep('basicBlockToAST', [Vertex,Pipe], { _().out('IS_BASIC_BLOCK_O
 Gremlin.defineStep('idToNode', [Vertex,Pipe], { _().transform{ g.v(it) }.scatter() })
 Gremlin.defineStep('queryToNodes', [Vertex,Pipe], { _().transform{ queryNodeIndex(it) }.scatter() })
 
+// Gremlin.defineStep('symbolToDecl', [Vertex, Pipe],{
+//  _().in('DEF').filter{ it.type in ['IdentifierDecl', 'Parameter']}
+// })
+
 Gremlin.defineStep('symbolToDecl', [Vertex, Pipe],{
-  _().in('DEF').filter{ it.type in ['IdentifierDecl', 'Parameter']}
+  _().in('DEF').filter{ it.type in ['IdentifierDecl']}
 })
 
 Gremlin.defineStep('nameToTypeDecl', [Vertex,Pipe], {
