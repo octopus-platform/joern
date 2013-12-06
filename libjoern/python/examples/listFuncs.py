a226570b5b5c5b79867f57d83fefa7d82415f09c
fabs <fyamagu@gwdg.de>
2013-12-06 22:11:41 +0100

    Improved examples/listfuncs.py slightly.

--- libjoern/python/examples/listFuncs.py
+++ libjoern/python/examples/listFuncs.py
@@ -7,12 +7,18 @@ from libjoern import JoernSteps
 j = JoernSteps()
 
 # Get functions by name from index
+# or, get all functions if no name is specified.
 
-cmd = "queryNodeIndex('functionName:%s')" % (sys.argv[1])
+if len(sys.argv) == 2:
+    cmd = "queryNodeIndex('functionName:%s')" % (sys.argv[1])
+else:
+    cmd = "queryNodeIndex('type:Function')"
 
-cmd += ".sideEffect{ name = it.functionName; }.in('IS_FILE_OF').sideEffect{fname = it.filepath }";
-cmd += '.transform{ [name, fname] }.toList()'
-
+cmd += """
+.sideEffect{ name = it.functionName; }
+.in('IS_FILE_OF').sideEffect{fname = it.filepath }
+.transform{ [name, fname] }.toList()
+"""
 
 y = j.executeGremlinCmd(cmd)
 
