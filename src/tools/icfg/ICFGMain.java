449006094785c243a1fc677fec97db67162a39f6
fabs <fyamagu@gwdg.de>
2013-12-17 11:38:57 +0100

    Added missing command line parameter to ICFG.

--- src/tools/icfg/ICFGMain.java
+++ src/tools/icfg/ICFGMain.java
@@ -1,5 +1,7 @@
 package tools.icfg;
 
+import org.apache.commons.cli.ParseException;
+
 import tools.GraphDbWalker;
 import tools.ImportedNodeListener;
 import tools.ImportedNodeWalker;
@@ -9,15 +11,36 @@ public class ICFGMain extends GraphDbWalker {
 	static ImportedNodeWalker walker = new ImportedNodeWalker();
 	static ImportedNodeListener listener = new ICFGListener();
 	
+	private static ICFGCommandLineInterface cmd = new ICFGCommandLineInterface();
+	
 	public static void main(String[] args)
 	{
-		initializeDatabase();
+		parseCommandLine(args);
+		setDatabaseDirectory(cmd.getDatabaseDir());
 		
+		initializeDatabase();
+				
 		walker.setType("CallExpression");
 		walker.addListener(listener);
 		walker.walk();
 		
 		shutdownDatabase();
 	}	
+	
+	private static void parseCommandLine(String [] args)
+	{
+		try{
+			cmd.parseCommandLine(args);
+		} catch(RuntimeException | ParseException ex){
+			printHelpAndTerminate(ex);
+		}
+	}
+	
+	private static void printHelpAndTerminate(Exception ex)
+	{
+		System.err.println(ex.getMessage());
+		cmd.printHelp();
+		System.exit(1);
+	}
 	
 }
