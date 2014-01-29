5cf784c01443787b2981cd8871e5998195ca44e8
fabs <fyamagu@gwdg.de>
2014-01-29 23:20:29 +0100

    Patch to fix issue #5.

--- src/astnodes/builders/ShadowStack.java
+++ src/astnodes/builders/ShadowStack.java
@@ -44,7 +44,7 @@ public class ShadowStack {
 	{
 		ASTNode topOfItemStack = itemStack.peek();
 		
-		if(stack.size() > 0 && stack.peek().parentCompound == topOfItemStack){
+		while(stack.size() > 0 && stack.peek().parentCompound == topOfItemStack){
 			stack.pop();
 		}
 	}
