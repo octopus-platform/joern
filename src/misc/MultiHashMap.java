e2534a5ead6c4e676a0f0fbcce1b7c9648ab3cfc
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-04-26 22:48:45 +0200

    MultiHashMap is generic now. Added new methods to MultiHashMap.

--- src/misc/MultiHashMap.java
+++ src/misc/MultiHashMap.java
@@ -7,72 +7,199 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 
-public class MultiHashMap
-{
-	public HashMap<Object, List<Object>> hashMap =
-			new HashMap<Object, List<Object>>();
-	
-	public Iterator<Entry<Object, List<Object>>> getEntrySetIterator()
-	{
-		return hashMap.entrySet().iterator();
-	}
-	
-	public Iterator<Object> getKeySetIterator()
-	{
-		return hashMap.keySet().iterator();
-	}
-	
-	public void add(Object key, Object val)
-	{
-		List<Object> valList = hashMap.get(key);
-		if(valList == null){
-			valList = new LinkedList<Object>();
-			hashMap.put(key, valList);
-		}
-		valList.add(val);
-	}
+public class MultiHashMap<K, V> {
 
-	public void addMultiHashMap(MultiHashMap otherHashMap)
-	{
-		Set<Entry<Object, List<Object>>> entrySet = otherHashMap.hashMap.entrySet();
-		Iterator<Entry<Object, List<Object>>> it = entrySet.iterator();
-		while(it.hasNext()){
-			Entry<Object, List<Object>> pair = it.next();
-			Iterator<Object> it2 = pair.getValue().iterator();
-			while(it2.hasNext()){
-				add(pair.getKey(), it2.next());
-			}
-		}
+    private HashMap<K, List<V>> hashMap = new HashMap<K, List<V>>();
+
+
+    public Set<Entry<K, List<V>>> entrySet() {
+	return hashMap.entrySet();
+    }
+
+
+    public Set<K> keySet() {
+	return hashMap.keySet();
+    }
+
+
+    public boolean containsKey(K key) {
+	return hashMap.containsKey(key);
+    }
+
+
+    public Iterator<Entry<K, List<V>>> getEntrySetIterator() {
+	return entrySet().iterator();
+    }
+
+
+    public Iterator<K> getKeySetIterator() {
+	return keySet().iterator();
+    }
+
+
+    /**
+     * Associates the specified value with the specified key. If the map
+     * previously contained a mapping for the key, the new value is appended.
+     * 
+     * @param key
+     *            key with which the specified value is to be associated
+     * 
+     * @param values
+     *            value to be associated with the specified key
+     */
+    public void add(K key, V value) {
+	if (containsKey(key)) {
+	    hashMap.get(key).add(value);
+	} else {
+	    List<V> list = new LinkedList<V>();
+	    list.add(value);
+	    hashMap.put(key, list);
+
 	}
-	
-	public void remove(Object key, Object val)
-	{
-		List<Object> dstList = hashMap.get(key);
-		if(dstList == null)
-			return;
-		dstList.remove(val);
+    }
+
+
+    /**
+     * Associates the specified values with the specified key. If the map
+     * previously contained a mapping for the key, the new values are appended.
+     * 
+     * @param key
+     *            key with which the specified values are to be associated
+     * 
+     * @param values
+     *            values to be associated with the specified key
+     */
+    public void add(K key, List<V> values) {
+	if (containsKey(key)) {
+	    hashMap.get(key).addAll(values);
+	} else {
+	    hashMap.put(key, values);
 	}
-	
-	public void removeAllForKey(Object key)
-	{
-		hashMap.put(key, new LinkedList<Object>());
+    }
+
+
+    public void addAll(MultiHashMap<K, V> otherMap) {
+	for (Entry<K, List<V>> entry : otherMap.entrySet()) {
+	    add(entry.getKey(), entry.getValue());
 	}
-	
-	public int size()
-	{
-		int s = 0;
-		Set<Entry<Object, List<Object>>> entrySet = hashMap.entrySet();
-		Iterator<Entry<Object, List<Object>>> it = entrySet.iterator();
-		while(it.hasNext()){
-			Entry<Object, List<Object>> pair = it.next();
-			s += pair.getValue().size();
-		}
-		return s;
+    }
+
+
+    @Deprecated
+    /**
+     * @deprecated
+     * Use addAll instead.
+     */
+    public void addMultiHashMap(MultiHashMap<K, V> otherHashMap) {
+	Set<Entry<K, List<V>>> entrySet = otherHashMap.entrySet();
+	Iterator<Entry<K, List<V>>> it = entrySet.iterator();
+	while (it.hasNext()) {
+	    Entry<K, List<V>> pair = it.next();
+	    Iterator<V> it2 = pair.getValue().iterator();
+	    while (it2.hasNext()) {
+		add(pair.getKey(), it2.next());
+	    }
 	}
-	
-	public List<Object> getListForKey(Object k)
-	{
-		return hashMap.get(k);
+    }
+
+
+    /**
+     * Removes the value from list associated with the specified key if it
+     * exists.
+     * 
+     * @param key
+     * @param value
+     * @return true if the map was modified otherwise false.
+     */
+    public boolean remove(K key, V value) {
+	if (containsKey(key)) {
+	    return get(key).remove(value);
 	}
-	
+	return false;
+	// List<V> dstList = hashMap.get(key);
+	// if (dstList == null)
+	// return false;
+	// dstList.remove(val);
+	// return true;
+    }
+
+
+    @Deprecated
+    /**
+     * @deprecated
+     * Use removeAll instead.
+     */
+    public void removeAllForKey(K key) {
+	hashMap.put(key, new LinkedList<V>());
+    }
+
+
+    /**
+     * Removes the mapping for the specified key from this map if present.
+     * 
+     * @param key
+     *            key whose mapping is to be removed from the map
+     * @return the previous value associated with key, or null if there was no
+     *         mapping for key.
+     */
+    public List<V> removeAll(K key) {
+	return hashMap.remove(key);
+    }
+
+
+    @Deprecated
+    /**
+     * @deprecated
+     * Use totalSize instead.
+     */
+    // this could return hashMap.size()
+    public int size() {
+	int s = 0;
+	Set<Entry<K, List<V>>> entrySet = hashMap.entrySet();
+	Iterator<Entry<K, List<V>>> it = entrySet.iterator();
+	while (it.hasNext()) {
+	    Entry<K, List<V>> pair = it.next();
+	    s += pair.getValue().size();
+	}
+	return s;
+    }
+
+
+    /**
+     * Calculates the total count of all values in this map.
+     * 
+     * @return the total count of all values.
+     */
+    public int totalSize() {
+	int size = 0;
+	for (List<V> values : hashMap.values()) {
+	    size += values.size();
+	}
+	return size;
+    }
+
+
+    @Deprecated
+    /**
+     * @deprecated
+     * Use get instead.
+     */
+    public List<V> getListForKey(K k) {
+	return hashMap.get(k);
+    }
+
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     * 
+     * @param key
+     *            the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or null if this
+     *         map contains no mapping for the key
+     */
+    public List<V> get(K key) {
+	return hashMap.get(key);
+    }
+
 }
