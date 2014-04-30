45ed4b3eaa26070c61f14d584d932cefc03a2be2
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-04-30 11:31:11 +0200

    Preliminary implementation

--- src/cdg/CDG.java
+++ src/cdg/CDG.java
@@ -2,4 +2,21 @@ package cdg;
 
 public class CDG {
 
+    private DominatorTree<Object> dominatorTree;
+
+
+    private CDG() {
+    }
+
+
+    public DominatorTree<Object> getDominatorTree() {
+	return this.dominatorTree;
+    }
+
+
+    public static CDG newInstance(DominatorTree<Object> dominatorTree) {
+	CDG cdg = new CDG();
+	cdg.dominatorTree = dominatorTree;
+	return cdg;
+    }
 }
