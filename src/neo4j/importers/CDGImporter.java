254d8d8791d9e9ec82a29720429833c9dd6701e8
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2014-04-30 11:36:29 +0200

    Preliminary implementation (imports only post-dominator edges)

--- src/neo4j/importers/CDGImporter.java
+++ src/neo4j/importers/CDGImporter.java
@@ -1,19 +1,37 @@
 package neo4j.importers;
 
-import cdg.CDG;
+import neo4j.EdgeTypes;
 import neo4j.batchInserter.GraphNodeStore;
+import neo4j.batchInserter.Neo4JBatchInserter;
+
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.RelationshipType;
+
+import cdg.CDG;
+import cdg.DominatorTree;
 
 public class CDGImporter {
 
-	public CDGImporter(GraphNodeStore nodeStore)
-	{
-		// TODO Auto-generated constructor stub
-	}
+    GraphNodeStore nodeStore;
 
-	public void addCDGToDatabase(CDG cdg)
-	{
-		// TODO Auto-generated method stub
-		
+
+    public CDGImporter(GraphNodeStore nodeStore) {
+	this.nodeStore = nodeStore;
+    }
+
+
+    public void addCDGToDatabase(CDG cdg) {
+
+	// Add post dominator edges
+	DominatorTree<Object> dom = cdg.getDominatorTree();
+	RelationshipType rel = DynamicRelationshipType.withName(EdgeTypes.POST_DOM);
+	for (Object vertex : dom.getVertices()) {
+
+	    long srcId = nodeStore.getIdForObject(dom.getDominator(vertex));
+	    long dstId = nodeStore.getIdForObject(vertex);
+
+	    Neo4JBatchInserter.addRelationship(srcId, dstId, rel, null);
 	}
+    }
 
 }
