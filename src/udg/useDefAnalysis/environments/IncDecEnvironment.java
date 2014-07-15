87e8e37940a3851846abe62ef17191bf432f7962
fabs <fyamagu@gwdg.de>
2014-07-15 08:20:05 +0200

    Fix #22 : IncDecOp now also have USE/DEF edges just like AssignmentExprs. Prior to this commit, they were missing USE-edges while DEF-edges were already present.

--- src/udg/useDefAnalysis/environments/IncDecEnvironment.java
+++ src/udg/useDefAnalysis/environments/IncDecEnvironment.java
@@ -7,17 +7,6 @@ public class IncDecEnvironment extends EmitDefEnvironment
 	@Override
 	public boolean isUse(ASTProvider child)
 	{
-		int childNum = child.getChildNumber();
-
-		if (childNum == 0)
-		{
-			String operatorCode = astProvider.getOperatorCode();
-			if (operatorCode != null)
-				return true;
-			else
-				return false;
-		}
-
 		return true;
 	}
 
