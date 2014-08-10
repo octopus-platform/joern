350797385971b4e6d4b74310477628c340a25abe
fabs <fyamagu@gwdg.de>
2014-08-10 23:48:31 +0200

    Removed misleading outdated comment.

--- src/cfg/StructuredFlowVisitor.java
+++ src/cfg/StructuredFlowVisitor.java
@@ -15,13 +15,6 @@ import astnodes.statements.SwitchStatement;
 import astnodes.statements.WhileStatement;
 import astwalking.ASTNodeVisitor;
 
-// This visitor is used to create a CFG from an AST
-// while considering only structured control flow statements,
-// e.g., for, while, do.
-
-// Once the CFG has been created using this Visitor, the
-// JumpStatementVisitor is used to account for unstructured
-// control flow statements, e.g., return, goto, continue, break.
 
 class StructuredFlowVisitor extends ASTNodeVisitor
 {
