beec23f7645a0096d80eae3e6b7690b40293dc31
fabs <fyamagu@gwdg.de>
2014-11-04 17:16:28 +0100

    New test code.

--- rebuildTestDB.sh
+++ rebuildTestDB.sh
@@ -10,4 +10,4 @@ java -jar bin/joern.jar testCode -outdir .testDB
 
 java -jar bin/argumentTainter.jar -dbdir .testDB taint_source 1
 java -jar bin/argumentTainter.jar -dbdir .testDB second_taint_source 0
-
+java -jar bin/argumentTainter.jar -dbdir .testDB interproc_callee 0
--- testCode/argumentTainterTest.c
+++ testCode/argumentTainterTest.c
@@ -4,3 +4,15 @@ int arg_tainter_basic_test()
   struct myVar;
   memset(myVar, 0, sizeof(myVar));
 }
+
+int interproc_arg_tainter_test()
+{
+  int x;
+  interproc_callee(&x);
+  sink12(x);
+}
+
+int interproc_callee(int *x)
+{
+  *x = source12();
+}
