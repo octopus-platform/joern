85f351e2389649ebb0f32a2043b0f59b6f44e85a
fabs <fyamagu@gwdg.de>
2015-09-23 18:09:42 +0200

    Build target for phpast2cfg.

--- src/tools/phpast2cfg/Main.java
+++ src/tools/phpast2cfg/Main.java
@@ -1,7 +1,12 @@
 package tools.phpast2cfg;
 
+import java.io.IOException;
+
 import org.apache.commons.cli.ParseException;
 
+import ast.functionDef.FunctionDef;
+import cfg.CFG;
+import inputModules.csv.KeyedCSV.exceptions.InvalidCSVFile;
 import inputModules.csv.csv2ast.CSV2AST;
 import languages.php.cfg.PHPCFGFactory;
 import outputModules.common.Writer;
@@ -13,7 +18,7 @@ public class Main
 	static CSV2AST csv2astConverter = new CSV2AST();
 	static PHPCFGFactory cfgFactory = new PHPCFGFactory();
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InvalidCSVFile, IOException
 	{
 		Writer.setWriterImpl(new CSVWriterImpl());
 
@@ -22,10 +27,8 @@ public class Main
 		String nodeFilename = cmdLine.getNodeFile();
 		String edgeFilename = cmdLine.getNodeFile();
 
-		// FunctionDef ast = csv2astConverter.convert(nodeFilename,
-		// edgeFilename);
-		// CFG cfg = cfgFactory.newInstance(ast);
-
+		FunctionDef ast = csv2astConverter.convert(nodeFilename, edgeFilename);
+		CFG cfg = cfgFactory.newInstance(ast);
 	}
 
 	private static void parseCommandLine(String[] args)
