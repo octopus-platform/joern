6b397718953b88f0e8d34212ff28d709715547f6
Malte Skoruppa <skoruppa@cs.uni-saarland.de>
2015-11-23 15:19:46 +0100

    Refactored C world to use a catch list abstraction
    C world was refactored to implicitly use a CatchList of CatchStatement's instead of having a TryStatement manage its CatchStatement children directly.
    I say "implicit" because the "outer world" in C does not need to instantiate a CatchList, instead a CatchList is implicitly created whenever a TryStatement is created. So basically this modification only affects how catch statement children are managed *internally* by a TryStatement in C world. The only difference is that getChildCount() should now normally be 2, instead of arbitrary: a statement and a catch list. However, all tests are still passing, I assume the number of children of a TryStatement was never tested in C world.
    
    Additionally, I also changed the line
      if (tryStatement.getCatchNodes() == null)
    to be
      if (tryStatement.getCatchList().size() == 0)
    in CCFGFactory, since I assume that's what was meant. Indeed, previously TryStatement always declared
      private List<CatchStatement> catchNodes = new LinkedList<CatchStatement>();
    so tryStatement.getCatchNodes() would *never* be equal to null and therefore the code after the if statement was unreachable.

--- src/ast/statements/blockstarters/TryStatement.java
+++ src/ast/statements/blockstarters/TryStatement.java
@@ -1,9 +1,5 @@
 package ast.statements.blockstarters;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import ast.ASTNode;
 import ast.logical.statements.BlockStarter;
 import ast.logical.statements.CompoundStatement;
 import ast.walking.ASTNodeVisitor;
@@ -11,7 +7,7 @@ import ast.walking.ASTNodeVisitor;
 public class TryStatement extends BlockStarter
 {
 	private CompoundStatement content = null;
-	private CatchList catchList = null;
+	private CatchList catchList = new CatchList();
 	private CompoundStatement finallyContent = null;
 	
 	public CompoundStatement getContent()
@@ -52,63 +48,4 @@ public class TryStatement extends BlockStarter
 	{
 		visitor.visit(this);
 	}
-	
-	/*
-	 * TODO the fields catchNodes as well as the associated methods below should
-	 * be removed. It would be best for the outer world to only use getCatchList()
-	 * above, and use whatever methods the class CatchList offers, instead of having
-	 * the following methods handle catch nodes directly.
-	 */
-	private List<CatchStatement> catchNodes = new LinkedList<CatchStatement>();
-
-	public void addCatchNode(CatchStatement catchNode)
-	{
-		getCatchNodes().add(catchNode);
-	}
-
-	public List<CatchStatement> getCatchNodes()
-	{
-		return this.catchNodes;
-	}
-
-	public CatchStatement getCatchNode(int index)
-	{
-		return getCatchNodes().get(index);
-	}
-
-	/*
-	 * TODO exactly as with IfStatement which also overrides the
-	 * methods getChildCount() and getChild(int), the following two
-	 * overrides should be moved to an extending class CTryStatement or
-	 * similar since their behavior is inconsistent with the usual behavior
-	 * of these methods in ASTNode and confuses PHP world. What's worse, in
-	 * this case I cannot even create a PHPTryStatement that extends TryStatement
-	 * as there is no good reason for this except to restore the original behavior
-	 * of these methods, but that would be the wrong way to go about it (rather,
-	 * CTryStatement should override them on its own if it needs to.)
-	 * See comments in PHPIfStatement.
-	 */
-	
-	@Override
-	public int getChildCount()
-	{
-		return super.getChildCount() + getCatchNodes().size();
-	}
-
-	@Override
-	public ASTNode getChild(int i)
-	{
-		if (i == 0)
-			return getStatement();
-		else
-			try
-			{
-				return getCatchNode(i - 1);
-			} catch (IndexOutOfBoundsException e)
-			{
-				throw new RuntimeException(
-						"Invalid child number for try statement");
-			}
-	}
-
 }
