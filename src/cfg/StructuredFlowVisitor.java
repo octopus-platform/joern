3fcd28c2fd20a8ee81ade0782122164905ce23fb
fabs <fyamagu@gwdg.de>
2016-01-05 11:18:19 +0100

    Connected PHPAST2CFG to CFG creator.
    The PHPAST2CFG tool now calls our original CFG creator, which now
    needs to be adapted via sub-classing to handle language specifics.

--- src/cfg/StructuredFlowVisitor.java
+++ src/cfg/StructuredFlowVisitor.java
@@ -1,4 +1,4 @@
-package languages.c.cfg;
+package cfg;
 
 import ast.ASTNode;
 import ast.functionDef.Parameter;
@@ -17,25 +17,27 @@ import ast.statements.jump.GotoStatement;
 import ast.statements.jump.ReturnStatement;
 import ast.statements.jump.ThrowStatement;
 import ast.walking.ASTNodeVisitor;
-import cfg.CFG;
 import cfg.nodes.ASTNodeContainer;
 import cfg.nodes.CFGNode;
+import languages.c.cfg.CCFGFactory;
 
-class StructuredFlowVisitor extends ASTNodeVisitor
+public class StructuredFlowVisitor extends ASTNodeVisitor
 {
 
 	private CFG returnCFG;
 
-	CFG getCFG()
+	public CFG getCFG()
 	{
 		return returnCFG;
 	}
 
+	@Override
 	public void visit(ParameterList paramList)
 	{
 		returnCFG = CCFGFactory.newInstance(paramList);
 	}
 
+	@Override
 	public void visit(Parameter param)
 	{
 		returnCFG = CCFGFactory.newInstance(param);
@@ -49,66 +51,79 @@ class StructuredFlowVisitor extends ASTNodeVisitor
 
 	}
 
+	@Override
 	public void visit(CompoundStatement content)
 	{
 		returnCFG = CCFGFactory.newInstance(content);
 	}
 
+	@Override
 	public void visit(ASTNode expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
+	@Override
 	public void visit(ReturnStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
+	@Override
 	public void visit(GotoStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
+	@Override
 	public void visit(IfStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(ForStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(WhileStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(DoStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(SwitchStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(Label node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
+	@Override
 	public void visit(ContinueStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
+	@Override
 	public void visit(BreakStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
+	@Override
 	public void visit(TryStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
--- src/languages/php/cfg/PHPCFGFactory.java
+++ src/languages/php/cfg/PHPCFGFactory.java
@@ -1,14 +1,8 @@
 package languages.php.cfg;
 
-import ast.functionDef.FunctionDef;
-import cfg.CFG;
 import cfg.CFGFactory;
 
 public class PHPCFGFactory extends CFGFactory
 {
-	@Override
-	public CFG newInstance(FunctionDef functionDefinition)
-	{
-		return null;
-	}
+
 }
