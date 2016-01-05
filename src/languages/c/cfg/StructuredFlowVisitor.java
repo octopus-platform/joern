d0784544948300c4770528908ba065e03fb4e323
Fabian Yamaguchi <fabs@navi.lan>
2016-01-05 22:30:07 +0100

    Revert "Connected PHPAST2CFG to CFG creator."
    This reverts commit 3fcd28c2fd20a8ee81ade0782122164905ce23fb.

--- src/languages/c/cfg/StructuredFlowVisitor.java
+++ src/languages/c/cfg/StructuredFlowVisitor.java
@@ -1,4 +1,4 @@
-package cfg;
+package languages.c.cfg;
 
 import ast.ASTNode;
 import ast.functionDef.Parameter;
@@ -17,27 +17,25 @@ import ast.statements.jump.GotoStatement;
 import ast.statements.jump.ReturnStatement;
 import ast.statements.jump.ThrowStatement;
 import ast.walking.ASTNodeVisitor;
+import cfg.CFG;
 import cfg.nodes.ASTNodeContainer;
 import cfg.nodes.CFGNode;
-import languages.c.cfg.CCFGFactory;
 
-public class StructuredFlowVisitor extends ASTNodeVisitor
+class StructuredFlowVisitor extends ASTNodeVisitor
 {
 
 	private CFG returnCFG;
 
-	public CFG getCFG()
+	CFG getCFG()
 	{
 		return returnCFG;
 	}
 
-	@Override
 	public void visit(ParameterList paramList)
 	{
 		returnCFG = CCFGFactory.newInstance(paramList);
 	}
 
-	@Override
 	public void visit(Parameter param)
 	{
 		returnCFG = CCFGFactory.newInstance(param);
@@ -51,79 +49,66 @@ public class StructuredFlowVisitor extends ASTNodeVisitor
 
 	}
 
-	@Override
 	public void visit(CompoundStatement content)
 	{
 		returnCFG = CCFGFactory.newInstance(content);
 	}
 
-	@Override
 	public void visit(ASTNode expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
-	@Override
 	public void visit(ReturnStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
-	@Override
 	public void visit(GotoStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
-	@Override
 	public void visit(IfStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(ForStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(WhileStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(DoStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(SwitchStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(Label node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
 	}
 
-	@Override
 	public void visit(ContinueStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
-	@Override
 	public void visit(BreakStatement expression)
 	{
 		returnCFG = CCFGFactory.newInstance(expression);
 	}
 
-	@Override
 	public void visit(TryStatement node)
 	{
 		returnCFG = CCFGFactory.newInstance(node);
