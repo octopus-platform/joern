b6886b150949a6b98c3247c8cf36f5023580d7f3
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-06-17 11:30:12 +0200

    git subrepo pull python/octopus-pylib
    subrepo:
      subdir:   "python/octopus-pylib"
      merged:   "2454286"
    upstream:
      origin:   "git@github.com:octopus-platform/octopus-pylib.git"
      branch:   "master"
      commit:   "2454286"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "cb2995b"

---
+++ python/octopus-pylib/octopus/server/orientdb/orientdb_plugin_executor.py
@@ -0,0 +1,14 @@
+import json
+
+from octopus.server.orientdb.orientdb_server_command import OrientDBServerCommand
+
+
+class OrientDBPluginExecutor(object):
+    def __init__(self, server_host, server_port):
+        self.command = OrientDBServerCommand(server_host, server_port)
+
+    def execute(self, pluginname, classname, settings=None):
+        data = {"plugin": pluginname, "class": classname, "settings": settings}
+        json_data = json.dumps(data)
+        print(json_data)
+        self.command.execute_post_command("/executeplugin/", json_data)
---
+++ python/octopus-pylib/octopus/server/orientdb/orientdb_project_manager.py
@@ -0,0 +1,25 @@
+import base64
+
+from octopus.server.orientdb.orientdb_server_command import OrientDBServerCommand
+from octopus.server.project_manager import ProjectManager
+
+
+class OrientDBProjectManager(ProjectManager):
+    def __init__(self, server_host, server_port):
+        self.command = OrientDBServerCommand(server_host, server_port)
+
+    def create(self, project_name):
+        self.command.execute_get_command("/manageprojects/create/{}".format(project_name))
+
+    def delete(self, project_name):
+        self.command.execute_get_command("/manageprojects/delete/{}".format(project_name))
+
+    def upload_file(self, project_name, file):
+        file_content = file.read()
+        content_type = "text/plain;charset=us/ascii"
+
+        name = "/uploadfile/{}/binary".format(project_name)
+        body = base64.b64encode(file_content)
+        headers = {"Content-type": content_type}
+
+        self.command.execute_post_command(name, body, headers)
