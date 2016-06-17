90a5893c7d164167ef42214cb5e0a3a46c3afcf7
Malte Skoruppa <skoruppa@cs.uni-saarland.de>
2016-06-17 11:19:25 +0200

    Renamed package tools.php.ast2cfgddg to tools.php.ast2cpg.

--- projects/joern-php/src/test/java/tests/languages/php/inputModules/PHPCSV2ASTTest.java
+++ projects/joern-php/src/test/java/tests/languages/php/inputModules/PHPCSV2ASTTest.java
@@ -15,8 +15,8 @@ import ast.php.functionDef.PHPFunctionDef;
 import inputModules.csv.KeyedCSV.exceptions.InvalidCSVFile;
 import inputModules.csv.csv2ast.CSV2AST;
 import inputModules.csv.csv2ast.PHPCSV2AST;
-import tools.php.ast2cfgddg.PHPCSVEdgeInterpreter;
-import tools.php.ast2cfgddg.PHPCSVNodeInterpreter;
+import tools.php.ast2cpg.PHPCSVEdgeInterpreter;
+import tools.php.ast2cpg.PHPCSVNodeInterpreter;
 
 public class PHPCSV2ASTTest
 {
