498b2aa0a0bc984fc474af385fba60101e8cb49c
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-06-27 16:28:36 +0200

    queryNodeIndex for joern-lang.

---
+++ projects/joern-lang/src/main/groovy/lookup.groovy
@@ -0,0 +1,6 @@
+queryNodeIndex = { luceneQuery ->
+
+	queryStr = 'SELECT * FROM V WHERE [childNum,code,filepath,functionId,key,location,name,type] LUCENE "' + luceneQuery + '"';
+	query = new com.orientechnologies.orient.core.sql.OCommandSQL(queryStr);
+	g.getRawGraph().command(query).execute().toList()._().transform{ g.v(it.getIdentity()) }
+}
