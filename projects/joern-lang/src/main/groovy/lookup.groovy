06af4a4fc87bf9961d136495f82ca772dd5303e4
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-06-27 16:51:35 +0200

    Adapt queryNodeIndex to `type`->`nodeType` renaming.

--- projects/joern-lang/src/main/groovy/lookup.groovy
+++ projects/joern-lang/src/main/groovy/lookup.groovy
@@ -1,6 +1,6 @@
 queryNodeIndex = { luceneQuery ->
 
-	queryStr = 'SELECT * FROM V WHERE [childNum,code,filepath,functionId,key,location,name,type] LUCENE "' + luceneQuery + '"';
+	queryStr = 'SELECT * FROM V WHERE [childNum,code,filepath,functionId,key,location,name,nodeType] LUCENE "' + luceneQuery + '"';
 	query = new com.orientechnologies.orient.core.sql.OCommandSQL(queryStr);
 	g.getRawGraph().command(query).execute().toList()._().transform{ g.v(it.getIdentity()) }
 }
