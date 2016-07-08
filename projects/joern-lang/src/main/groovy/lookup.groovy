19a240dd612244e563445afce64433d7eae55c64
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-07-08 20:33:29 +0200

    Call activateOnCurrentThread() in queryNodeIndex.

--- projects/joern-lang/src/main/groovy/lookup.groovy
+++ projects/joern-lang/src/main/groovy/lookup.groovy
@@ -7,7 +7,9 @@ queryNodeIndex = { def luceneQuery ->
 
 	def queryStr = 'SELECT * FROM V WHERE [childNum,code,filepath,functionId,isCFGNode,key,location,name,nodeType] LUCENE "' + luceneQuery + '"';
 	def query = new com.orientechnologies.orient.core.sql.OCommandSQL(queryStr);
-	g.getRawGraph().command(query).execute().toList()._().transform{ g.v(it.getIdentity()) }
+	def db = g.getRawGraph();
+	db.activateOnCurrentThread();
+	db.command(query).execute().toList()._().transform{ g.v(it.getIdentity()) }
 }
 
 /**
