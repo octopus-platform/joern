114e2ac23c1c79fe35c47ace70551c3c33e0ce86
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-08-07 23:45:02 +0200

    Adapt startup script.

--- projects/octopus/octopus-server/octopus-server.sh
+++ projects/octopus/octopus-server/octopus-server.sh
@@ -2,9 +2,7 @@
 
 
 MAXHEAP="-Xmx${OCTOPUS_SERVER_MAXHEAP:-512m}"
-MAXDISKCACHE="-Dstorage.diskCache.bufferSize=${OCTOPUS_SERVER_MAXDISKCACHE:-8192}"
-OPTS_FROM_ORIENTDB="-server -Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9 -Dprofiler.enabled=true"
-
+OPTS_FROM_ORIENTDB="-Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -XX:MaxDirectMemorySize=512g -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 OCTOPUS_HOME=`dirname $0`
 
-exec java $JAVA_OPTS $MAXHEAP $MAXDISKCACHE $OPTS_FROM_ORIENTDB -DOCTOPUS_HOME="$OCTOPUS_HOME" -cp "$OCTOPUS_HOME/jars/*" octopus.OctopusMain
+exec java $JAVA_OPTS $MAXHEAP $OPTS_FROM_ORIENTDB -DOCTOPUS_HOME="$OCTOPUS_HOME" -cp "$OCTOPUS_HOME/jars/*" octopus.OctopusMain
