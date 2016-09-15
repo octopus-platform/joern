9a320a48f037cc0f7b87a5d6564a947569dd6a73
Tim Hemel <tim@securesoftware.nl>
2016-09-15 16:24:27 +0200

    Fix syntax error

--- projects/languages/joern-lang/src/main/groovy/syntax/assignment.groovy
+++ projects/languages/joern-lang/src/main/groovy/syntax/assignment.groovy
@@ -4,9 +4,9 @@
 */
 
 GraphTraversal.metaClass.lval = {
-	delegate.out(AST_EDGE).has(TYPE_CHILDNUM, '0')
+	delegate.out(AST_EDGE).has(NODE_CHILDNUM, '0')
 }
 
 GraphTraversal.metaClass.rval = {
-	delegate.out(AST_EDGE).has(TYPE_CHILDNUM, '1')
+	delegate.out(AST_EDGE).has(NODE_CHILDNUM, '1')
 }
