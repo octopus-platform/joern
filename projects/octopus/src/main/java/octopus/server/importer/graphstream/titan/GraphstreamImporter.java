eb8e9b4bcdd6bf58e8faf0d91f32d785a46e0a9e
Tim Hemel <tim@securesoftware.nl>
2017-01-23 13:01:59 +0100

    changed pythonic string format function to java string format function. add implementations for all Graphstream events for purpose of debugging.

--- projects/octopus/src/main/java/octopus/server/importer/graphstream/titan/GraphstreamImporter.java
+++ projects/octopus/src/main/java/octopus/server/importer/graphstream/titan/GraphstreamImporter.java
@@ -56,14 +56,90 @@ public class GraphstreamImporter extends SinkAdapter {
 	}
 
 	@Override
-	public void nodeAdded(String sourceId, long timeId, String nodeId) {
-	    System.out.println("DGS: add node {} from source {}".format(nodeId,sourceId));
-		Vertex vertex = graph.addVertex(KEY, nodeId);
+	public void edgeAttributeAdded(String sourceId, long timeId, String edgeId,
+			String attribute, Object value) {
+		logger.warn("DGS: edgeAttributeAdded");
 	}
 
-	public void nodeAttributeAdded(String sourceId, long timeId, String nodeId) {
-		System.out.println("DGS: add node attribute nodeid={} sourceid={}".format(nodeId,sourceId));
+	@Override
+	public void edgeAttributeChanged(String sourceId, long timeId,
+			String edgeId, String attribute, Object oldValue, Object newValue) {
+		logger.warn("DGS: edgeAttributeChanged");
+	}
+
+	@Override
+	public void edgeAttributeRemoved(String sourceId, long timeId,
+			String edgeId, String attribute) {
+		logger.warn("DGS: edgeAttributeRemoved");
+	}
+
+	@Override
+	public void graphAttributeAdded(String sourceId, long timeId,
+			String attribute, Object value) {
+		logger.warn("DGS: graphAttributeAdded");
+	}
+
+	@Override
+	public void graphAttributeChanged(String sourceId, long timeId,
+			String attribute, Object oldValue, Object newValue) {
+		logger.warn("DGS: graphAttributeChanged");
+	}
+
+	@Override
+	public void graphAttributeRemoved(String sourceId, long timeId,
+			String attribute) {
+		logger.warn("DGS: graphAttributeRemoved");
+	}
+
+	@Override
+	public void nodeAttributeAdded(String sourceId, long timeId, String nodeId,
+			String attribute, Object value) {
+		logger.warn(String.format("DGS: add node attribute nodeid=%s attribute=%s",nodeId,attribute));
+		// logger.warn((String) value);
 		// vertex.property(keys[i - 1], row[i]);
 	}
 
+	@Override
+	public void nodeAttributeChanged(String sourceId, long timeId,
+			String nodeId, String attribute, Object oldValue, Object newValue) {
+		logger.warn("DGS: nodeAttributeChanged");
+	}
+
+	@Override
+	public void nodeAttributeRemoved(String sourceId, long timeId,
+			String nodeId, String attribute) {
+		logger.warn("DGS: nodeAttributeRemoved");
+	}
+
+	@Override
+	public void edgeAdded(String sourceId, long timeId, String edgeId,
+			String fromNodeId, String toNodeId, boolean directed) {
+		logger.warn("DGS: edgeAdded");
+	}
+
+	@Override
+	public void edgeRemoved(String sourceId, long timeId, String edgeId) {
+		logger.warn("DGS: edgeRemoved");
+	}
+
+	@Override
+	public void graphCleared(String sourceId, long timeId) {
+		logger.warn("DGS: graphCleared");
+	}
+
+	@Override
+	public void nodeAdded(String sourceId, long timeId, String nodeId) {
+	    logger.warn(String.format("DGS: add node %s from source %s",nodeId,sourceId));
+		// Vertex vertex = graph.addVertex(KEY, nodeId);
+	}
+
+	@Override
+	public void nodeRemoved(String sourceId, long timeId, String nodeId) {
+		logger.warn(String.format("DGS: nodeRemoved(%s,...,%s)",sourceId,nodeId));
+	}
+
+	@Override
+	public void stepBegins(String sourceId, long timeId, double step) {
+		logger.warn("DGS: stepBegins");
+	}
 }
